1aa01985fed0c76e88be5019039f0e7601dc8a75
jeejimon <jeehae.moon@gmail.com>
2021-02-17 18:02:10 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+
+hamburger.addEventListener('click',() => {
+    
+      navLinks.classList.toggle("open");
+
+})
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+navLinks.addEventListener('click', () => {
+  navLinks.classList.toggle("open");
+})
